058b142c2d773922c39cba60f0fdf2620a2d94a1
Natasha A D Rocha <rsaron_nat@hotmail.com>
2018-12-16 20:15:13 -0300

    ajustando script jenkinsfile, cucumber e env

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,5 +1,11 @@
 pipeline {
-    agent any
+    agent{
+        docker {
+            image 'ruby'
+            args '--link selenium'
+        }
+    }    
+
     environment {
         CI = true
     }
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,32 @@
 require 'capybara'
 require 'capybara/cucumber'
 
+@browser = ENV['BROWSER']
+
+if @browser.eql?('headless')
+    puts 'Executando com Headless'
+
+    Capybara.javascript_driver = :selenium 
+    Capybara.run_server = false
+
+    args = ['--no-default-browser-check']
+
+    caps = selenium::WebDriver::Remote::Capabilities.chrome{
+        'chromeOptions' => { 'args' => args }
+    }
+
+    Capybara.register_driver :selenium do |app|
+        Capybara::selenium::Driver.new{
+            app,
+            browser: :remote,
+            url: 'http://selenium:4444/wd/hub',
+            desired_capabilities: caps
+        }
+    end
+else
+    puts 'Executando sem Headless'
+end
+
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
     config.app_host = 'https://mark7.herokuapp.com'
